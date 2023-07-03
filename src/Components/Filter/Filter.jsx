1cebdcabe5f9d980ecd97f6b7b5cbf9198e23c6c
alhad-tm <alhadchikkutm@gmail.com>
2023-07-03 17:00:15 +0530

    working on filter slider

--- src/Components/Filter/Filter.jsx
+++ src/Components/Filter/Filter.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import MultiRangeSlider from "multi-range-slider-react";
+
 import css from "./Filter.module.css"
 
 const Filter = () => {
+    const [minValue, set_minValue] = useState(25);
+const [maxValue, set_maxValue] = useState(75);
+const handleInput = (e) => {
+	set_minValue(e.minValue);
+	set_maxValue(e.maxValue);
+};
+
   return (
-    <div className={css.container}>
+    <div className={css.container}> 
 
       <div className={css.div1}>
         <span>Filter</span>
@@ -45,7 +54,24 @@ const Filter = () => {
         </div>
       </div>
 
-
+<div className={css.div3}>
+    <div className={css.div3top}>
+        <span>Price Range</span>
+        <span>RS 0 - 100k</span>
+    </div>
+    <div className={css.div3bottom}>
+    <MultiRangeSlider
+			min={1200}
+			max={10000}
+			step={500}
+			minValue={minValue}
+			maxValue={maxValue}
+			onInput={(e) => {
+				handleInput(e);
+			}}
+		/>
+    </div>
+</div>
 
 
     </div>
